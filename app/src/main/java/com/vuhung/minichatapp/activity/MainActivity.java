aa05f5f03f70a18c56df523f80994e86632a6b6a
huuvuno1 <nguyenhuuvuno1@gmail.com>
2021-09-18 12:24:49 +0700

    checkouit

--- app/src/main/java/com/vuhung/minichatapp/activity/MainActivity.java
+++ app/src/main/java/com/vuhung/minichatapp/activity/MainActivity.java
@@ -1,13 +1,25 @@
 package com.vuhung.minichatapp.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.vuhung.minichatapp.R;
+import com.vuhung.minichatapp.fragment.ChatFragment;
+import com.vuhung.minichatapp.fragment.UsersFragment;
 import com.vuhung.minichatapp.socket.MySocket;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    TextView titleNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // start socket
         MySocket.start();
+        titleNav = findViewById(R.id.title_home);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(handleNavSelected);
+        bottomNavigationView.setSelectedItemId(R.id.tab_chat);
+
+    }
+
+    private NavigationBarView.OnItemSelectedListener handleNavSelected = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.tab_chat:
+                    fragment = new ChatFragment();
+                    loadFragment(fragment);
+                    titleNav.setText("Chat");
+                    return true;
+                case R.id.tab_users:
+                    fragment = new UsersFragment();
+                    titleNav.setText("All User");
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_main, fragment);
+        // transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/vuhung/minichatapp/fragment/UsersFragment.java
@@ -0,0 +1,24 @@
+package com.vuhung.minichatapp.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vuhung.minichatapp.R;
+
+public class UsersFragment extends Fragment {
+    public UsersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_users, container, false);
+    }
+}
